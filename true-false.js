a9c8afead5dd69348b3017f7b72e4136ec197b89
ishtiakanam <iafahim68@gmail.com>
2020-12-10 22:06:45 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,17 @@
+//0 - false
+//NaN - false
+//undefined - false
+//null - false
+//"" - false
+
+//" " - true
+// '0' - true
+
+const name = "Ishtiak anam";
+
+if(name.length > 0) {
+    console.log("condition is true");
+}
+else {
+    console.log("condition is false");~
+}

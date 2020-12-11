427a4feccd38c95c5c36fce0475c13b6dfabb790
ishtiakanam <iafahim68@gmail.com>
2020-12-11 07:26:52 +0600

    scope work

---
+++ scope.js
@@ -0,0 +1,21 @@
+
+let bonus = 20;
+
+function sum(first, second){
+    let result = first + second + bonus;
+     console.log(bonus);
+    if(result > 9){
+        let mood = "happy";
+        mood = "for";
+        mood = "passing";
+        mood = "exam";
+        console.log(mood);
+    }
+    var day = "friday"
+    console.log(day);
+    return result;
+}
+console.log(mood);
+const output = sum(3,7);
+console.log(bonus);
+console.log(output);
